982f0531a77c04a4fc835ca91dff9c065b440de4
Michi Hirczy <mi.hi@gmx.net>
2024-07-19 23:13:25 +0200

    RConsIOC: working mcp code

--- IOController/ioControllerRCONS/ioControllerRCONS.ino
+++ IOController/ioControllerRCONS/ioControllerRCONS.ino
@@ -75,13 +75,13 @@ uint16_t registerMcpPrevious[MCPNUM] = { 0, 0, 0 ,0 };
 // bit array with all button states
 boolean joyButtons[JOYBUTTONS] = { 0 };
 
- struct InputMapping
+ typedef struct InputMapping
 {
   byte btnNum;
   boolean isSingle;  // does the input create only one button (rotary switch) or multiple (2/3way switch)
   boolean nextPositive; // if multiple, is the "off" button number +1 or -1 than btnNum
-};
-
+} InputMapping;
+/*
 // Define all inputs per mcp and their according joystick button values (on/off)
 InputMapping aircondButtons[MCPINPUTS] = {
   // AIRCOND Rotary
@@ -187,8 +187,110 @@ InputMapping kyButtons[MCPINPUTS] = {
 
   {NOTUSED, 0, 0}  // Pin 16 not used
 };
+*/
+const InputMapping allMappings[MCPNUM][MCPINPUTS] = {
+  {    ////////////////////  AIRCONDITION  //////////////////////////
 
-const InputMapping allMappings[][MCPNUM] = { {aircondButtons}, {avpwrButtons}, {hudButtons}, {kyButtons} };
+    {AIRCONDSTART,   1, 0}, // OFF (0)
+    {AIRCONDSTART+1, 1, 0 }, // NORM (1)
+    {AIRCONDSTART+2, 1, 0}, // DUMP (2)
+    {AIRCONDSTART+3, 1, 0}, // RAM  (3)
+
+    // NUCLEAR
+    {AIRCONDSTART+4, 0, 1}, // CRAD1 (4) -> CRAD2 (5)
+
+    {AIRCONDSTART+6, 0, 1}, // REL ONLY (6) -> OFF (7)
+    {AIRCONDSTART+8, 0, 0}, // ARM/REL (8) -> OFF (7)
+
+    {NOTUSED, 0, 0}, // pin 7 not used
+
+    // SNSPWR
+    {AIRCONDSTART+9,  0, 1}, // LEFT HDPT (9)-> OFF (10)
+
+    {AIRCONDSTART+11, 0 ,1}, // RIGHT HDPT (11)-> OFF (12)
+
+    {AIRCONDSTART+13, 0, 1}, // FCR (13)-> OFF (14)
+
+    {AIRCONDSTART+15, 0, 1}, // RADAR ALT (15)-> STDBY (16)
+    {AIRCONDSTART+17, 0, 0}, // OFF (17)-> STDBY (16)
+
+    {NOTUSED, 0, 0},
+    {NOTUSED, 0, 0},
+    {NOTUSED, 0, 0}
+  },
+  {    ////////////////////   HUD   ////////////////////////
+
+    {HUDSTART,   0, 1}, // Pin 0, VV/VAH -> VAH (1)
+    {HUDSTART+2, 0, 0}, // Pin 1, OFF -> VAH (1)
+
+    {HUDSTART+3, 0, 1}, // Pin 2, ATT/FPM -> FPM (4)
+    {HUDSTART+5, 0, 0}, // Pin 3, OFF -> FPM (4)
+
+    {HUDSTART+6, 0, 1}, // Pin 4, DED DATA -> PFD (7)
+    {HUDSTART+8, 0, 0}, // Pin 5, OFF -> PFD (7)
+
+    // DEPR RET
+    {HUDSTART+9,  0, 1}, // Pin 6, STBY -> PRI (10)
+    {HUDSTART+11, 0, 0}, // Pin 7, OFF -> PRI (10)
+
+    {HUDSTART+12, 0, 1}, // Pin 8, CAS -> TAS (13)
+    {HUDSTART+14, 0, 0}, // Pin 9, OFF -> TAS (13)
+
+    {HUDSTART+15, 0, 1}, // Pin 10, ALT RADAR -> BARO (16)
+    {HUDSTART+17, 0, 0}, // Pin 11, OFF -> BARO (16)
+
+    {HUDSTART+18, 0, 1}, // Pin 12, DAY (18) -> AUTO BRT (19)
+    {HUDSTART+20, 0, 0}, // Pin 13, NIGHT (20) -> AUTO BRT (19)
+
+    {HUDSTART+21, 0, 1}, // Pin 10, TEST STEP -> ON (22)
+    {HUDSTART+23, 0, 0} // Pin 11, OFF -> ON (22)
+  },
+  {  /////////////////////  AVPWR  ///////////////////////
+    {AVPWRSTART,   1, 0}, // OFF
+    {AVPWRSTART+1, 1, 0 }, // STOR HDG
+    {AVPWRSTART+2, 1, 0}, // NORM
+    {AVPWRSTART+3, 1, 0}, // NAV
+    {AVPWRSTART+4, 1, 0}, // CAL
+    {AVPWRSTART+5, 1, 0}, // IN FLT ALIGN
+    {AVPWRSTART+6, 1, 0}, // ATT
+    // MIDS LVT
+    {AVPWRSTART+7, 0, 1}, // ZERO -> OFF (8)
+    {AVPWRSTART+9, 0, 0}, // ON-> OFF (8)
+
+    {AVPWRSTART+10, 0, 1}, // MMC -> OFF (11)
+    {AVPWRSTART+12, 0, 1}, // ST STA -> OFF (13)
+    {AVPWRSTART+14, 0, 1}, // MFD -> OFF (15)
+    {AVPWRSTART+16, 0, 1}, // UFC -> OFF (17)
+    {AVPWRSTART+18, 0, 1}, // MAP -> OFF (19)
+    {AVPWRSTART+20, 0, 1}, // DL -> OFF (21)
+    {AVPWRSTART+22, 0, 1} // GPS -> OFF (23)
+  },
+  { /////////////////////  KY58  /////////////////////////
+    // MODE rotary
+    {KYSTART,   1, 0}, // P
+    {KYSTART+1, 1, 0}, // C
+    {KYSTART+2, 1, 0}, // LD
+    {KYSTART+3, 1, 0}, // RV
+
+    // TD Rotary
+    {KYSTART+4, 0, 1}, // TD -> On
+    {KYSTART+6, 0, 0}, // OFF -> On
+
+    {NOTUSED, 0, 0}, // Pin 7 not used
+
+    //FILL Rotary
+    {KYSTART+7,  1, 0}, // Z 1-5
+    {KYSTART+8,  1, 0}, // 1
+    {KYSTART+9,  1, 0}, // 2
+    {KYSTART+10, 1, 0}, // 3
+    {KYSTART+11, 1, 0}, // 4
+    {KYSTART+12, 1, 0}, // 5
+    {KYSTART+13, 1, 0}, // 6
+    {KYSTART+14, 1, 0}, // Z ALL
+
+    {NOTUSED, 0, 0}  // Pin 16 not used
+  }
+ };
 
 /* struct mcpInputs {
   InputMapping inputMappings[MCPINPUTS];
@@ -206,9 +308,9 @@ mcpInputs MCPInputs[MCPNUM] = {ac, av, hud, ky};
 // ffffffffffffffffffffffffffffffff functions
 
 void setMCPButtonValue(int mcp, int input) {
-/*
+
   boolean inputValue = !(mcps[mcp].digitalRead(input)); // get state of changed input, negate because of pinned to gnd
-  InputMapping mapping = MCPInputs[mcp].inputMappings[input];
+  InputMapping mapping = allMappings[mcp][input];
   byte baseBtnNum = mapping.btnNum;
   if (baseBtnNum != 255) {
     Serial.print("input ");Serial.print(input);Serial.print(" value: "); Serial.print(inputValue);Serial.print(" BaseButton: ");Serial.println(baseBtnNum);
@@ -229,7 +331,6 @@ void setMCPButtonValue(int mcp, int input) {
     }
     joyButtons[baseBtnNum] = inputValue;
   }
-  */
 }
 
 
@@ -337,8 +438,8 @@ void setup() {
   pinMode(OXYEMERPIN, INPUT_PULLUP);
 
   Joystick.begin(false);
-  for (int mcp=0; mcp<MCPNUM; mcp++) {
-    //Serial.print("mcp: ");Serial.println(mcp);
+  /*for (int mcp=0; mcp<MCPNUM; mcp++) {
+    Serial.print("---------------------  mcp: ");Serial.println(mcp);
     for (int input=0; input<MCPINPUTS; input++) {
       InputMapping mapping = allMappings[mcp][input];
       Serial.println("============");
@@ -349,7 +450,7 @@ void setup() {
       Serial.println("============");
     }
 
-  }
+  }*/
 
   Serial.println("Setup finished");
 
@@ -358,7 +459,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   //checkAntiIce();
-  //checkMCPs();
+  checkMCPs();
   updateJoystick();
   delay(300); // only input reading does not need high performance, let chip rest
  //
